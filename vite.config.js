415cc04ab41df3aa0237db189c249e0add9fdfb8
Dazai211 <arjaysarmiento44@gmail.com>
2025-10-27 08:38:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: "/ias-test",
+  base: "/ias-test/",
   server: {
     host: '0.0.0.0',
   },
